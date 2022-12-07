0a934055e43fc0376e50cb8662014bfa73621ddc
alizare-1986 <alianayatzare.a.a.1986@gmail.com>
2022-12-07 09:28:25 +0330

    make project cryptocurrency & fetch api from server

---
+++ src/components/Landing.js
@@ -0,0 +1,21 @@
+import React,{useEffect,} from 'react';
+import { getCoin } from '../services/api';
+
+const Landing = () => {
+
+    useEffect(()=> {
+        const fetchAPI = async () => {
+          const data = await getCoin();
+          console.log(data)
+        }
+        fetchAPI()
+
+        },[])
+    return (
+        <div>
+            landing
+        </div>
+    );
+};
+
+export default Landing;
